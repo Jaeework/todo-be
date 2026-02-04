e5a216ad1ecbb511ae124d587042adfac063791f
Jaeework <jaeework@gmail.com>
2026-02-05 01:11:28 +0900

    feat: initial server setup with express and mongo db
    - Setup express server on port 5000
    - Configure mongodb connection to local database
    - Add body-parser middleware for JSON parsing

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const mongoURI = `mongodb://localhost:27017/todo-demo`;
+
+mongoose.connect(mongoURI)
+    .then(() => { 
+        console.log("mongoose connected") 
+    })
+    .catch((err) => {
+        console.log("DB connection failed", err);
+    });
+
+app.listen(5000, () => {
+    console.log("server on 5000");
+});
